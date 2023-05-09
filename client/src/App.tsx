d9bdb8e12e8875d907fae78808ea19b5b978600b
jesskreck <krecker.jessica@gmail.com>
2023-05-09 14:51:34 +0200

    first init

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
+export default function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ } />
+        <Route path="login" element={ } />
+        <Route path="chat" element={ } />
+      </Routes>    
+    </BrowserRouter>
+  )
+}
